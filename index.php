c69368eb1dd92b3915e87e1c4b157392a6b95b9b
Erik Hansen <erik@classyllama.com>
2009-09-19 19:57:10 -0500

    Conflicting merge with expiriment!!!

--- index.php
+++ index.php
@@ -8,6 +8,8 @@ modified master
  * NOTICE OF LICENSE
  *iasdfvadfsvafv
 
+2nd mod to index.php
+
  * This source file is subject to the Open Software License (OSL 3.0)
  * that is bundled with this package in the file LICENSE.txt.
  * It is also available through the world-wide-web at this URL:
