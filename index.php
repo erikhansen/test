3426e3c416e2269c0eb9dd8930b0b78c6c017e22
Erik Hansen <erik@classyllama.com>
2009-09-19 19:48:01 -0500

    added file and modified index.php

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
  * Magento
  *
  * NOTICE OF LICENSE
- *
+ *iasdfvadfsvafv
+
  * This source file is subject to the Open Software License (OSL 3.0)
  * that is bundled with this package in the file LICENSE.txt.
  * It is also available through the world-wide-web at this URL:
