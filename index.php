c343bed09c22eb4e0eb540cf1951288e748d8df0
Erik Hansen <erik@classyllama.com>
2009-09-19 19:55:47 -0500

    MOdded index.php

--- index.php
+++ index.php
@@ -8,7 +8,9 @@ modified master
  * NOTICE OF LICENSE
  *iasdfvadfsvafv
 
- * This source file is subject to the Open Software License (OSL 3.0)
+second mod to expiriment 
+
+* This source file is subject to the Open Software License (OSL 3.0)
  * that is bundled with this package in the file LICENSE.txt.
  * It is also available through the world-wide-web at this URL:
  * http://opensource.org/licenses/osl-3.0.php
